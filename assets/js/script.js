7d91417c4058db3a9d886e36e9545d047ce445d5
jeremyljr <jeremyjiaren@gmil.com>
2021-06-17 16:37:35 +0100

    loading animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,13 @@ timeline
         .addTo(controller);
 
 
-console.log((window.innerHeight*2.5))
+gsap.fromTo(".me", {opacity:0, x:-100},{duration: 1, opacity:1 ,delay: 0.7 ,ease:"power3.out", x: 0});
+gsap.fromTo(".sea", {opacity:0, y:100},{duration: 1, opacity:1 ,delay: 0.7 ,ease:"power3.out", y: 0});
+gsap.fromTo(".nav", {opacity:0, y:-100},{duration: 1, opacity:1 ,delay: 0.7 ,ease:"power3.out", y: 0});
+gsap.fromTo(".leaves", {opacity:1, x:300},{duration: 1, opacity:1 ,delay: 1.5 ,ease:"power3.out", x: 0});
+gsap.fromTo(".mainRight", {opacity:0, x:100},{duration: 1, opacity:1 ,delay: 2 ,ease:"power3.out", x: 0});
+gsap.fromTo(".mainTitle", {opacity:0, x:-100},{duration: 1, opacity:1 ,delay: 1.5 ,ease:"power3.out", x: 0});
+
 //Smaooth scroll
 function scrollClickAbout() {
     window.scroll(0, (window.innerHeight*2.5))
